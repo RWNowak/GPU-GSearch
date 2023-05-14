e80c83f1189391adb7bec3acc3b610ff11899210
Rafał Nowak <rwojciechnowak@gmail.com>
2023-05-14 15:31:26 +0200

    feat: retrieve covers from IGDB API

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, jsonify, request
+from urllib.parse import urljoin
 import mysql.connector
+from flask import Flask, render_template, jsonify, request
+from igdb.igdbapi_pb2 import GameResult
+from igdb.wrapper import IGDBWrapper
+import json
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -11,6 +15,35 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 app = Flask(__name__)
+
+## CODE FOR API'S
+
+wrapper = IGDBWrapper('ed7hmod9n4zphsnxhr3n10hab2h296','97p1lbcr39cvkwswygawm9aq2fkhbm')
+
+# Search for ID by name
+
+def get_game_id(name):
+    byte_array = wrapper.api_request(
+            'games.pb',
+            f'fields id, name; search "{name}"; where version_parent = null;'
+          )
+    games_message = GameResult()
+    games_message.ParseFromString(byte_array) 
+    game_id = games_message.games[0].id
+    return game_id
+    
+#Get cover URL
+
+def get_game_cover(game_id):
+    byte_array = wrapper.api_request(
+                'covers',
+                f'fields url; where id = {game_id};'
+            )
+    json_response = json.loads(byte_array.decode())
+    url = json_response[0]['url']
+    return url
+    
+## ROUTES
 
 @app.route('/')
 def index():
@@ -25,8 +58,9 @@ def search():
     results = cursor.fetchone()
     name = results[0]
     release_date = results[1]
-    return render_template('index.html', name=name, release_date=release_date )
-
+    game_id = get_game_id(search_term)
+    cover_url = get_game_cover(game_id)
+    return render_template('index.html', name=name, release_date=release_date, cover_url=cover_url)
 # @app.route('/autocomplete')
 # def autocomplete():
     query = request.args.get('query')
@@ -34,3 +68,5 @@ def search():
     cursor.execute(sql, ('%' + query + '%',))
     results = cursor.fetchall()
     return jsonify([result[0] for result in results])
+
+
